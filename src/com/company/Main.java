ebae420f445a722896dbd89a4b444413e359b266
Denisa <denisabirisan@yahoo.com>
2020-12-09 20:42:34 +0200

    Tema Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,18 @@ public class Main {
 	// write your code here
         System.out.println("Hello Denisa!");
         System.out.println("Hello again!");
+        System.out.println("Hello!");
+        System.out.println("Denisa :)");
+        System.out.println(5+5);
+        int x=8/2;
+        System.out.println(x);
+        double f1=10.0/3.0;
+        System.out.println(f1);
+        int y=-5 + 8 * 6;
+        System.out.println(y);
+        System.out.println((55+9) % 9);
+        double f2=20.0 + -3.0*5.0 / 8.0;
+        System.out.println(f2);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
     }
 }
