a72de304834090d30c18269799995992fc9aa9f0
Denisa <denisabirisan@yahoo.com>
2020-12-13 14:36:39 +0200

    Tema Metode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,145 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Denisa!");
-        System.out.println("Hello again!");
-        System.out.println("Hello!");
-        System.out.println("Denisa :)");
-        System.out.println(5+5);
-        int x=8/2;
-        System.out.println(x);
-        double f1=10.0/3.0;
-        System.out.println(f1);
-        int y=-5 + 8 * 6;
-        System.out.println(y);
-        System.out.println((55+9) % 9);
-        double f2=20.0 + -3.0*5.0 / 8.0;
-        System.out.println(f2);
-        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+        printMyName();
+        int res=sum(5,5);
+        System.out.println(res);
+        double result=div(10,3);
+        System.out.println(result);
+        res=sumtimes(-5,8,6);
+        System.out.println(res);
+        res=summodulo(55,9,9);
+        System.out.println(res);
+        result=a(20,-3,5,8);
+        System.out.println(result);
+        result=b(5,15,3,2,8,3);
+        System.out.println(result);
+        System.out.println(b2());
+        System.out.println("Rezultatul adunarii este: " + adunare(2,3));
+        System.out.println("Rezultatul scaderii este: " + scadere(1,3));
+        System.out.println("Rezultatul inmultirii este: " + inmultire(4,3));
+        System.out.println("Rezultatul impartirii este: " + impartire(10,3));
+
+        printFace();
+        prinJava();
+
+        res=rest(10,7);
+        System.out.println("Restul impartirii este:" + res);
+
+        result=celsius(70);
+        System.out.println("Grade in Celsius: " + result);
+
+        result=meters(500.3);
+        System.out.println(result + " metri");
+
+        result=viteza(1609,10,10,10);
+        System.out.println(result);
+
+
+
+
+
+
+
     }
-}
+
+    static void printMyName(){
+        System.out.println("Hello!\nDenisa");
+    }
+
+    static int sum(int firstNumber, int secondNumber){
+        return(firstNumber + secondNumber);
+    }
+
+    static double div(double firstNumber, double secondNumber){
+        return (firstNumber / secondNumber);
+    }
+
+    static int sumtimes(int firstNumber, int secondNumber, int thirdNumber){
+        return(firstNumber+secondNumber*thirdNumber);
+    }
+
+    static int summodulo(int firstNumber, int secondNumber, int thirdNumber){
+        return((firstNumber+secondNumber)%thirdNumber);
+    }
+
+    static double a(double firstNumber, double secondNumber, double thirdNumber, double fourthNumber){
+        return(firstNumber+(secondNumber*thirdNumber)/fourthNumber);
+    }
+
+    static double b(double firstNumber, double secondNumber, double thirdNumber, double fourthNumber, double fifthNumber, double sixthNumber){
+        return(firstNumber+(secondNumber/thirdNumber)*fourthNumber-fifthNumber%sixthNumber);
+    }
+
+    static double b2(){
+        return(5 + 15 / 3 * 2 - 8 % 3 );
+    }
+
+    public static int adunare(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar + alDoileaNumar;
+
+        return rezultat;
+    }
+    public static int scadere(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar - alDoileaNumar;
+
+        return rezultat;
+    }
+    public static int inmultire(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar*alDoileaNumar;
+
+        return rezultat;
+    }
+    public static double impartire(double primulNumar, double alDoileaNumar) {
+        double rezultat = primulNumar/alDoileaNumar;
+        return rezultat;
+    }
+
+    static void printFace(){
+        System.out.println(" +\"\"\"\"\"+ \n" +
+                "[| o o |]\n" +
+                " |  ^  |\n " +
+                "| '-' |\n " +
+                "+-----+\n ");
+
+    }
+
+    static void prinJava(){
+        System.out.println("   J     a    v     v   a\n"+
+                           "   J    a a    v   v   a a\n"+
+                           "J  J   aaaaa    v v   aaaaa\n" +
+                           " JJ   a     a    v   a     a");
+    }
+
+
+    static int rest(int primulNumar, int alDoileaNumar){
+        return(primulNumar%alDoileaNumar);
+    }
+
+    static double celsius(double primulNumar){
+        double result=((primulNumar -32)*5/9);
+        return result;
+    }
+
+    static double meters(double primulNumar){
+        return (primulNumar*0.0254);
+    }
+
+    static double viteza (double metri, double ore, double minute, double secunde){
+        double a=metri/secunde;
+        double b=((metri/1000)/ore);
+        double c=((metri/1609)/ore);
+        System.out.println(a);
+        System.out.println(b);
+        return (c);
+
+
+    }
+
+    }
+
+
---
+++ src/com/company/lab2/Lab2.java
@@ -0,0 +1,85 @@
+package com.company.lab2;
+
+public class Lab2 {
+    public static void main(String[] args) {
+
+        int result=sum(3,3);
+        System.out.println(result);
+
+        result=sub(10,3);
+        System.out.println(result);
+
+        result=div(10,5);
+        System.out.println(result);
+
+        result=times(3,5);
+        System.out.println(result);
+
+        result=rest(20,3);
+        System.out.println(result);
+
+        double result2=real(5,4);
+        System.out.println(result2);
+
+        result2=bigsum(2,3,4.3);
+        System.out.println(result2);
+
+        int d=dif(3,5);
+        System.out.println(d);
+
+        double impresult=imp(10,2,4);
+        System.out.println(impresult);
+
+        double aresult=a(10,3,4);
+        System.out.println(aresult);
+
+
+
+
+
+
+    }
+    static int sum(int firstNumber, int secondNumber){
+        return(firstNumber + secondNumber);
+    }
+
+    static int sub(int firstNumber, int secondNumber) {
+        return (firstNumber - secondNumber);
+    }
+
+    static int div(int firstNumber, int secondNumber) {
+        return (firstNumber / secondNumber);
+    }
+
+    static int times(int firstNumber, int secondNumber){
+        return( firstNumber*secondNumber);
+    }
+
+    static int rest(int firstNumber, int secondNumber){
+        return(firstNumber%secondNumber);
+    }
+
+    static double real(double firstNumber, double secondNumber){
+        return(firstNumber/secondNumber);
+    }
+
+    static double bigsum(int firstNumber, int secondNumber, double thirdNumber){
+        return(firstNumber+secondNumber+thirdNumber);
+    }
+
+    static int dif(int firstNumber, int secondNumber){
+        return(firstNumber-secondNumber);
+    }
+
+    static double imp(double firstNumber, double secondNumber, double thirdNumber){
+        return(firstNumber/secondNumber/thirdNumber);
+    }
+
+    static double a(double firstNumber, double secondNumber, double thirdNumber){
+        return(firstNumber/secondNumber+thirdNumber);
+    }
+
+
+
+
+}
